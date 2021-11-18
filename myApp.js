4cbbd29064ec6ec2f38132fbac18c637e269e6bc
Irwing Naranjo <khalisser@gmail.com>
2021-11-17 23:28:31 -0300

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -6,10 +6,13 @@ const BASEPATH = __dirname;
 app.use('/public', express.static(`${BASEPATH}/public`));
 
 app.get('/', (req, res) => {
-
     const file = `${BASEPATH}/views/index.html`;
-
     res.sendFile(file);
+});
+
+app.get('/json', (req, res) => {
+    const response = { "message": "Hello json" };
+    res.json(response);
 });
 
 module.exports = app;
