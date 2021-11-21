8b0169f5c72001e4d024bb796ab0267fb0d353d5
Irwing Naranjo <khalisser@gmail.com>
2021-11-21 19:07:14 -0300

    Use body-parser to Parse POST Requests

--- myApp.js
+++ myApp.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 const BASEPATH = __dirname;
 
